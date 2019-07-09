3146c6c7f15aa99e28363d605021a8540dc78bd8
李贺鑫 <lihexin88@github.com>
2019-07-09 17:06:33 +0800

    test

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -28,6 +28,7 @@ class SessionsController extends Controller {
             session()->flash('success', '欢迎回来！');
             return redirect()->route('users.show', [Auth::user()]);
         } else {
+//            a
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
         }
