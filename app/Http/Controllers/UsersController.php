09c3f6e1ebc52889c9c969df67ddd2bcc448216d
vagrant <vagrant@homestead>
2019-07-09 07:32:55 +0000

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,22 +5,35 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 
-class UsersController extends Controller
-{
+class UsersController extends Controller {
     //
-    public function create(){
+    public function create() {
         return view('users.create');
     }
-    public function show(User $user){
-       return view('users.show',compact('user'));
+
+    public function show(User $user) {
+        return view('users.show', compact('user'));
     }
-    public function store(Request $request)
-    {
+
+    /**
+     * 保存用户并重新定向
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request) {
         $this->validate($request, [
             'name' => 'required|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-        return;
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
     }
 }
