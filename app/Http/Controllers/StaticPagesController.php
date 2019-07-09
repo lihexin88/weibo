3671f7fbdea82fe3c58a8bd8310897ea285239de
vagrant <vagrant@homestead>
2019-07-09 02:27:03 +0000

    静态页面

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -3,17 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use phpDocumentor\Reflection\Types\Void_;
 
 class StaticPagesController extends Controller {
-    //
-    /**
-     * @return string
-     */
-    public function help() {
-        return "用户帮助";
+    public function home() {
+        return view('static_pages/home');
     }
-    public function about(){
-        return "关于页面";
+
+    public function help() {
+        return view('static_pages/help');
+    }
+
+    public function about() {
+        return view('static_pages/about');
     }
 }
---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title','关于')
+@section('content')
+    <h1>关于页面</h1>
+@stop
+
