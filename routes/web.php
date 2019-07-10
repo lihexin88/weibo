7772d818e5c2ac7e7b75563c65d417b47519b18a
vagrant <vagrant@homestead>
2019-07-10 06:14:07 +0000

    关注和粉丝列表页面

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 
 
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+
+
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
